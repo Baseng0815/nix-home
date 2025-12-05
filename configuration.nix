3e3652f7a415ff189b90601f2eead06ef51aead8
Bastian Engel <bastian.engel00@gmail.com>
2025-12-05 16:20:13 +0100

    update hostname

--- configuration.nix
+++ configuration.nix
@@ -45,7 +45,7 @@
     polkit.enable = true;
   };
 
-  networking.hostName = "enlap"; # Define your hostname.
+  networking.hostName = "endesk"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
