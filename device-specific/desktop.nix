db10803d126b5c035b3997c2d5b7a4111345289a
Bastian Engel <bastian.engel00@gmail.com>
2026-03-30 23:09:58 +0200

    add desktop device

---
+++ device-specific/desktop.nix
@@ -0,0 +1,10 @@
+{ lib, pkgs, ... }:
+
+{
+  wayland.windowManager.hyprland.extraConfig = ''
+    monitor=DP-5,2560x1440@165,0x0,1
+    monitor=DP-4,1920x1080@144,2560x0,1
+  '';
+
+  programs.waybar.settings.mainBar.output = [ "DP-5" "DP-4" ];
+}
