4f9879cbac152419396d8db18fbbc18aa2765364
Bastian Engel <bastian.engel00@gmail.com>
2026-05-21 07:21:20 +0200

    remove hardware include from laptop configuration

--- device-specific/laptop-01/configuration.nix
+++ device-specific/laptop-01/configuration.nix
@@ -5,11 +5,6 @@
 { config, pkgs, ... }:
 
 {
-  imports = [
-    # Include the results of the hardware scan.
-    ./hardware-configuration.nix
-  ];
-
   hardware = {
     graphics.enable = true;
     graphics.enable32Bit = true;
